28f9163a45de5986fb008e60ae09d448a21a707d
OnvoKoder <65452318+OnvoKoder@users.noreply.github.com>
2020-11-05 18:35:00 +0300

    My UI design (portfolio)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var btn=document.getElementsByClassName("btn");
+for(let i=0;i<btn.length;i++){
+	btn[i].addEventListener('click', function(){
+		let current=document.getElementsByClassName("active");
+		current[0].className=current[0].className.replace("active", "");
+		this.className+=" active";
+	});
+}
+var picture=document.getElementById("picture");
+	btn[0].onclick=function(){
+		picture.src="images/pic1.png";
+	}
+	btn[1].onclick=function(){
+		picture.src="images/pic2.png";
+	};
+	btn[2].onclick=function(){
+		picture.src="images/pic3.png";
+	};
+	btn[3].onclick=function(){
+		picture.src="images/pic4.png";
+	};
